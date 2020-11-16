6392bb0e0defc41412d2db0325d3f2e38f599312
abiru <masataka.abiru@gmail.com>
2020-11-16 13:18:58 +0900

    change: readme

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-# Next.js + Tailwind CSS
+## What is this? 
 
-This is an example of using [Tailwind CSS](https://tailwindcss.com) in a Next.js project.
+This is the open source e-commerce template based on fatlightslim.com 
+
+- pre-rendered (for SEO) and be very fast.
+- generates HTML and JSON files, both of which can be cached by a CDN for performance.
+- 18n
+
+## When should I use it?
+
+If you are attempting to launch a seo frindly web site, this is it!
+
+## Who is it for?
+
+You are supposed to have a knowledge about `react.js` and  `css`.
+
+# Why?
+
+Our site is by standing on the shoulders of giants.
+http://fatlightslim.com/thanks
 
 ## How to use
 
 ```bash
-git clone https://github.com/abiru/nextjs-with-tailwind.git myproject
-cd myproject
+git clone https://github.com/fatlightslim/web.git fatlightslim-clone
+cd fatlightslim-clone
 npm i
 npm run dev
 ```
@@ -31,8 +48,7 @@ Deploy it to the cloud with [Vercel](https://vercel.com/import?filter=next.js&ut
     <!-- - https://nerdcave.com/tailwind-cheat-sheet -->
     - https://github.com/aniftyco/awesome-tailwindcss
 
-
 - SVG Icon
-    https://heroicons.com
+    - https://heroicons.com
 
 
--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,47 @@
-import Nav from './Nav'
+import Nav from "./Nav2"
 import Head from "next/head"
 import Footer from "./Footer"
 
 export default function Layout({
   children,
-  title = "This is the default title",
+  data = {
+    title: "植物用ライト専門店 FAT Light SLIM",
+    desc:
+      "海外で人気の植物栽培用ライトを安心価格で安全にお届けします。ホビー用途から商用向けまでお取り扱い中！MARS HYDRO公式代理店。中古LEDライトの買取も随時受付中。",
+    href: "https://fatlightslim.com/",
+    color: "#006eff",
+    img:
+      "http://cdn.shopify.com/s/files/1/0470/9089/2956/files/Screen_Shot_2020-11-14_at_17.52.16_1200x1200.png?v=1605489192",
+    og: {
+      site_name: "FAT Light SLIM",
+      type: "website",
+    },
+    tw: {
+      card: "summary_large_image",
+    },
+  },
 }) {
   return (
     <div className="mx-auto antialiased">
       <Head>
-        <title>{title}</title>
+        <title>{data.title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content={data.color} />
+        <meta name="description" content={data.desc} />
+        <link rel="canonical" href={data.href} />
+        <meta property="og:site_name" content={data.og.site_name} />
+        <meta property="og:url" content={data.href} />
+        <meta property="og:title" content={data.title} />
+        <meta property="og:type" content={data.og.type} />
+        <meta property="og:description" content={data.desc} />
+
+        <meta property="og:image" content={data.img} />
+        <meta property="og:image:secure_url" content={data.img} />
+
+        <meta name="twitter:card" content={data.tw.card} />
+        <meta name="twitter:title" content={data.title} />
+        <meta name="twitter:description" content={data.desc} />
       </Head>
       <Nav />
 
