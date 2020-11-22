ac06df2f62cf3e351cd943a81424cd19cb02fde0
MinjiKinn <minjikinn819@gmail.com>
2020-11-22 16:53:19 +0900

    xxx

--- data/Reviewdata.js
+++ data/Reviewdata.js
@@ -1,9 +1,9 @@
-import icon from "./img/icon.png"
-import icon2 from "./img/icon2.png"
-import icon3 from "./img/icon3.png"
-import icon4 from "./img/icon4.png"
-import icon5 from "./img/icon5.png"
-import icon6 from "./img/icon6.png"
+import icon from "./public/img/icon.png"
+import icon2 from "./public/img/icon2.png"
+import icon3 from "./public/img/icon3.png"
+import icon4 from "./public/img/icon4.png"
+import icon5 from "./public/img/icon5.png"
+import icon6 from "./public/img/icon6.png"
 
 
 export const data = [
