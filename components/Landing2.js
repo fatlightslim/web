780ff29d979ba31970e497e9eeb2bb1ed00cee4f
MinjiKinn <minjikinn819@gmail.com>
2020-11-22 15:30:33 +0900

    add: Landing2

---
+++ components/Landing2.js
@@ -0,0 +1,105 @@
+export default function Landing2(params) {
+    return (
+        <head>
+
+        <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+            <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+
+            <title>FAT Light SLIM</title>
+
+        </meta>
+    </head>
+        ,
+        <body class="font-sans font-thin">
+
+            <div class="w-full h-screen relative text-white">
+                <img src="https://cdn.shopify.com/s/files/1/0470/9089/2956/files/91352.jpg?v=1605750211" class="absolute top-0 left-0 w-full h-full object-cover" />
+
+                <header class="z-10 relative mx-auto max-w-6xl px-6 py-8 flex justify-between items-center">
+
+                    <a href="#" class="text-xl tracking-wider">FAT Light SLIM</a>
+
+                    <ul class="hidden md:flex uppercase text-sm text-gray-500">
+                        
+                        <li>
+                            <a href="#" class="text-white">Home</a>
+                        </li>
+                        <li class="ml-6">
+                            <a href="#">About</a>
+                        </li>
+                        <li class="ml-6">
+                            <a href="https://fatlightslim.com/blogs/staff/how-to-choose-your-grow-light">Blog</a>
+                        </li>
+                        <li class="ml-6">
+                            <a href="https://fatlightslim.com/pages/contact">Contact</a>
+                        </li>
+                    </ul>
+
+                </header>
+
+                <div class="absolute top-0 left-0 w-full h-full flex justify-center items-center text-center px-12">
+                    <div>
+                        <h1 class="text-3xl md:text-6xl leading-tight mb-16">世界で一番売れてるライト！<br class="hidden md:block" />MARS HYDRO</h1>
+                        <a href="https://web-7apx08vo7.vercel.app/en/products" class="border border-white py-4 px-8 hover:bg-white hover:text-black">Now on Sale!</a>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+            <div class="py-24 px-6">
+
+                <div class="max-w-6xl mx-auto mb-12">
+
+
+                    <h2 class="font-bold mx-auto text-center text-2xl md:text-5xl max-w-3xl leading-tight text-gray-800 mb-12">屋内栽培の本場アメリカで<br /><span class="underline">9割以上</span>のお客様にご満足の評価を<br />いただいております！</h2>
+
+
+                    <p class="mb-6 text-gray-600 mx-auto max-w-lg text-center leading-relaxed">明るさ、熱調整、消音、電気消費量、値段まで！全ての部門で平均☆4.7点を獲得。「収穫量が十分ではない」「植物が急に弱った」等の原因のほとんどはライトの光量不足によるものです。「植物の元気のために電気代やブレーカー、故障のことは気にせずにライトを当て続けたい」そんな夢が叶う2020年最新版の植物用LEDライトをお届けします。</p>
+
+
+                    <p class="text-gray-600 mx-auto max-w-lg text-center leading-relaxed">色んなクーポンがもらえる！ <a href="https://www.instagram.com/invites/contact/?i=swf8p8ynuslc&amp;utm_content=kcc3nuo" class="text-black">Instagram</a>もチェック！</p>
+
+                </div>
+
+
+
+                <div class="max-w-5xl mx-auto flex flex-wrap items-start">
+
+                    <div class="w-full md:w-1/2 p-4 mb-12">
+                        <img src="https://cdn.shopify.com/s/files/1/0470/9089/2956/files/2062.jpg?v=1605753164" class="block w-full h-auto mb-12" />
+                        <a href="#" class="block text-2xl text-black text-center mb-6">収穫量にきっとビックリします</a>
+                        <p class="mb-6 text-gray-600 text-center leading-relaxed text-sm">太陽光のような心地よい光は植物の光合成をバッチリサポート！生き生きと日々成長するハーブや野菜を育てて、収穫の楽しさを味ってみるのはいかがでしょう！</p>
+                        <a href="#" class="block text-black text-center">Read more</a>
+                    </div>
+
+                    <div class="w-full md:w-1/2 p-4 mb-12">
+                        <img src="https://cdn.shopify.com/s/files/1/0470/9089/2956/files/1613.jpg?v=1605752742" class="block w-full h-auto mb-12" />
+                        <a href="#" class="block text-2xl text-black text-center mb-6">電気代がこんなにおトク</a>
+                        <p class="mb-6 text-gray-600 text-center leading-relaxed text-sm">明るいライトだったらやっぱり電気代が心配！Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+                        <a href="#" class="block text-black text-center">Read more</a>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div class="bg-gray-900 text-gray-100 text-center flex justify-center px-6 py-12">
+
+                
+                <a href="#" class="mx-6 hover:text-gray-600">0800 123 456</a>
+        
+
+            </div>
+
+
+        </body>
+
+    )
+  }
+  
