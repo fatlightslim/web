823979cf544202c3f1ac99c63dc872829fdf4f50
Abiru <abr@mini.local>
2020-11-25 10:10:30 +0900

    add: sitemap

---
+++ components/Testimonial.js
@@ -0,0 +1,19 @@
+export default function Testimonial(params) {
+  return (
+    <section className="py-12 bg-gray-50 overflow-hidden md:py-20 lg:py-24">
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="relative">
+          <blockquote className="mt-10">
+            <div className="max-w-3xl mx-auto text-center text-2xl leading-9 font-medium text-gray-900">
+              <p>
+                “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+                expedita voluptas culpa sapiente alias molestiae. Numquam
+                corrupti in laborum sed rerum et corporis.”
+              </p>
+            </div>
+          </blockquote>
+        </div>
+      </div>
+    </section>
+  );
+}
