a43c5c57d5d1b6dff28e026087b8602ca97fddfb
Abiru <abr@mini.local>
2020-11-26 19:43:51 +0900

    remove: OneMoreThing

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ function Home() {
       <ReviewBanner />
       <Refund />
       <FeatureSummary />
-      <OneMoreThing />
+      {/* <OneMoreThing /> */}
       {/* <PricingTable /> */}
       <RelatedProducts />
     </Layout>
