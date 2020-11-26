a27f1e9d08db7342caa7367c81d4938ff9b6d5c1
Abiru <abr@mini.local>
2020-11-26 18:48:41 +0900

    add: OneMoreThing

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import '../styles/index.css'
 import "slick-carousel/slick/slick.css";
 import "../styles/slick-theme.css";
+import { useRouter } from 'next/router'
+import * as gtag from '../scripts/gtag'
+import { useEffect } from 'react'
 
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps}   />
+  return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
