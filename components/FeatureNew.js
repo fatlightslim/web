51869d916054ff212450bf516836118deed53fc5
MinjiKinn <minjikinn819@gmail.com>
2020-11-30 11:57:01 +0900

    add: FeatureNew

---
+++ components/FeatureNew.js
@@ -0,0 +1,450 @@
+import Image from "next/image";
+import CheckMark from "./Checkmark";
+
+export default function FeatureNew({
+  data = {
+    title: "もう二度と植物を枯れさせない！",
+    sub: "",
+    1: "",
+    2: "",
+    3: "",
+  },
+}) {
+  return (
+    <>
+      {/* This example requires Tailwind CSS v2.0+ */}
+      <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
+        <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+          <div className="relative">
+            <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              これらの悩み、FAT Light SLIMならすべて解決できます！
+            </h2>
+            <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac
+              massa nunc. Vestibulum ante ipsum primis in faucibus orci luctus
+              et ultrices posuere cubilia curae; Ut hendrerit interdum ex ut
+              commodo.
+            </p>
+          </div>
+          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="relative">
+              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                もう二度と植物を枯れさせない！
+              </h3>
+              <p className="mt-3 text-lg text-gray-500">
+                第3世代LEDチップが室内栽培の常識を変えます。旧世代比で1.5倍の速度、2.5倍の収穫量を実現しました。太陽光のようなライト色はまるでハワイにいるかのような気分。旧来の赤・青・紫のライト色では把握することが難しかった植物の健康状態もカンタンにチェックできます。
+              </p>
+              <dl className="mt-10 space-y-10">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-500 text-white">
+                      {/* Heroicon name: globe-alt */}
+
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width={2}
+                          d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      最新のLEDチップを搭載
+                    </dt>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-500 text-white">
+                      {/* Heroicon name: scale */}
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width={2}
+                          d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      白色のライトで植物の状態を常にチェック
+                    </dt>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-500 text-white">
+                      {/* Heroicon name: lightning-bolt */}
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width={2}
+                          d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      成長速度も収穫量も桁違い
+                    </dt>
+                  </div>
+                </div>
+              </dl>
+            </div>
+            <div
+              className="mt-10 -mx-4 relative lg:mt-0 flex justify-center"
+              aria-hidden="true"
+            >
+              <svg
+                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+              >
+                <defs>
+                  <pattern
+                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={784}
+                  height={404}
+                  fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
+                />
+              </svg>
+              <Image
+                src="/img/hand.png"
+                alt="multi-env"
+                width={490}
+                height={490}
+              />
+            </div>
+          </div>
+
+          <div className="relative mt-12 sm:mt-16 lg:mt-24">
+            <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+              <div className="lg:col-start-2">
+                <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                  ファンなし。ノイズなし。かなりクール。
+                </h3>
+                <p className="mt-3 text-lg text-gray-500">
+                  従来のライトはLEDの発熱を逃がすために常時ファンが回り続ける構造になっています。MARS
+                  HYDROの製品は最新のLEDチップとアルミ製ヒートシンクを採用することでファンが不要な設計です。当然、電気代も安くなります。
+                </p>
+                <dl className="mt-10 space-y-10">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-500 text-white">
+                        {/* Heroicon name: annotation */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4 flex items-center">
+                      <dt className="text-lg leading-6 font-medium text-gray-900">
+                        従来のライトは常時ファンが回る構造
+                      </dt>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-500 text-white">
+                        {/* Heroicon name: mail */}
+
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width={2}
+                            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4 flex items-center">
+                      <dt className="text-lg leading-6 font-medium text-gray-900">
+                        ヒートシンクで発熱を抑えファンが不要
+                      </dt>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-yellow-500 text-white">
+                        {/* Heroicon name: mail */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width={2}
+                            d="M9 8l3 5m0 0l3-5m-3 5v4m-3-5h6m-6 3h6m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4 flex items-center">
+                      <dt className="text-lg leading-6 font-medium text-gray-900">
+                        当然、電気代も安くなります
+                      </dt>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+              <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1 flex justify-center items-center">
+                <svg
+                  className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                  width={784}
+                  height={404}
+                  fill="none"
+                  viewBox="0 0 784 404"
+                  aria-hidden="true"
+                >
+                  <defs>
+                    <pattern
+                      id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={784}
+                    height={404}
+                    fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)"
+                  />
+                </svg>
+                <Image
+                  src="/img/peace.png"
+                  alt="main background"
+                  width={490}
+                  height={490}
+                  quality={100}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+            <div className="relative">
+              <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                最適なライトがきっと見つかります
+              </h3>
+              <p className="mt-3 text-lg text-gray-500">
+                お部屋の一画での趣味の栽培から水耕栽培工場での商業用栽培まで様々な環境に対応してきたMARS
+                HYDROの製品なら、選べるサイズ、デイジーチェーン（数珠つなぎ）機能であなたの環境にとってベストなライト構成が実現できます。
+              </p>
+              <dl className="mt-10 space-y-10">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-400 text-white">
+                      {/* Heroicon name: globe-alt */}
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width={2}
+                          d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      選べるサイズであらゆる環境に適応
+                    </dt>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-400 text-white">
+                      {/* Heroicon name: scale */}
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width={2}
+                          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      デイジーチェーン対応
+                    </dt>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-400 text-white">
+                      {/* Heroicon name: lightning-bolt */}
+
+                      <svg
+                        className="h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M13 10V3L4 14h7v7l9-11h-7z"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex items-center">
+                    <dt className="text-lg leading-6 font-medium text-gray-900">
+                      ベストなライト構成が実現できます
+                    </dt>
+                  </div>
+                </div>
+              </dl>
+            </div>
+            <div
+              className="mt-10 -mx-4 relative lg:mt-0 flex justify-center"
+              aria-hidden="true"
+            >
+              <svg
+                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+              >
+                <defs>
+                  <pattern
+                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={784}
+                  height={404}
+                  fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)"
+                />
+              </svg>
+              <Image
+                src="/img/puzzle.png"
+                alt="main background"
+                width={490}
+                height={490}
+                quality={100}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
