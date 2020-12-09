05f85b50ef572c27bacdbc03fd2a570408092dc7
Abiru <abr@mini.local>
2020-12-09 12:02:32 +0900

    change: feature

---
+++ data/nayami.js
@@ -0,0 +1,47 @@
+export const reviews = [
+  {
+    img: "/img/review1.png",
+    bg: "bg-gray-50",
+    lead: "大好きで3つも使っている",
+    lead2: "肥料を使っても植物の成長が遅くて収穫量が少ない",
+    dq: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq.png?v=1604515690",
+    dq2: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq2.png?v=1604515689",
+    link: "https://fatlightslim.com/products/mars-hydro-sp3000"
+  },
+  {
+    img: "/img/review2.png",
+    bg: "bg-gray-50",
+    lead: "もう買ってしまえばいい",
+    lead2: "グリーンハウスで使いたいけどスプリンクラーで濡れるのが心配",
+    dq: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq.png?v=1604515690",
+    dq2: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq2.png?v=1604515689",
+    link: "https://fatlightslim.com/products/mars-hydro-sp3000"
+  },
+  {
+    img: "/img/review3.png",
+    bg: "bg-gray-50",
+    lead: "MARS HYDROを買って後悔することはないです",
+    lead2: "趣味でやってみたいけど電気代が怖い",
+    dq: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq.png?v=1604515690",
+    dq2: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq2.png?v=1604515689",
+    link: "https://fatlightslim.com/products/mars-hydro-ts-series"
+  },
+  {
+    img: "/img/review4.png",
+    bg: "bg-gray-50",
+    lead: "箱の中の太陽",
+    lead2: "冬になって日調量が足りなくなった",
+    dq: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq.png?v=1604515690",
+    dq2: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq2.png?v=1604515689",
+    link: "https://fatlightslim.com/products/mars-hydro-ts-series"
+  },
+  {
+    img: "/img/review0.png",
+    bg: "bg-gray-50",
+    lead: "すごく明るくて、静かで、熱くならない",
+    lead2: "似たような物を使ったことあるけど付けっぱなしだどすぐ壊れちゃう",
+    dq: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq.png?v=1604515690",
+    dq2: "https://cdn.shopify.com/s/files/1/0470/9089/2956/files/dq2.png?v=1604515689",
+    link: "https://fatlightslim.com/products/mars-hydro-ts-series"
+  },
+]
