228ba229609b9572e720024bee743c8ebe85c82d
abiru <masataka.abiru@gmail.com>
2021-01-27 22:45:45 +0900

    fix: css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,32 +8,29 @@ module.exports = {
       "./components/**/*.{js,ts,jsx,tsx}",
       "./pages/**/*.{js,ts,jsx,tsx}",
     ],
-      whitelistPatterns: [/^bg-/, /^text-/, /^from-/, /^via-/, /^to/],
-    // options: {
-    //   safelist: [
-    //     "bg-indigo-600",
-    //     "hover:bg-indigo-700",
-    //     "lg:col-start-6",
-    //     "lg:col-end-8",
-    //     "lg:row-start-2",
-    //     "lg:row-end-3",
-    //     "lg:mx-0",
-    //     "lg:max-w-none",
-    //     "lg:col-start-1",
-    //     "lg:row-start-2",
-    //     " lg:row-end-3",
-    //     "lg:col-end-3",
-    //     "from-gray-900",
-    //     "from-gray-800",
-    //     "from-gray-700",
-    //     "via-purple-900",
-    //     "via-red-900",
-    //     "via-green-900",
-    //     "to-indigo-900",
-    //     "to-yellow-900",
-    //     "to-green-800",
-    //   ],
-    // },
+    options: {
+      safelist: [
+        /^bg-/,
+        /^text-/,
+        /^from-/,
+        /^via-/,
+        /^to-/,
+        "hover:bg-indigo-700",
+      ],
+      // safelist: [
+      //   "hover:bg-indigo-700",
+      //   "lg:col-start-6",
+      //   "lg:col-end-8",
+      //   "lg:row-start-2",
+      //   "lg:row-end-3",
+      //   "lg:mx-0",
+      //   "lg:max-w-none",
+      //   "lg:col-start-1",
+      //   "lg:row-start-2",
+      //   "lg:row-end-3",
+      //   "lg:col-end-3",
+      // ],
+    },
   },
   theme: {
     extend: {
@@ -50,4 +47,4 @@ module.exports = {
   plugins: [
     // require("@tailwindcss/aspect-ratio")
   ],
-};
+}
